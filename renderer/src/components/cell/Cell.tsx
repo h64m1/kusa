494cb911fad406a5ac8f5c999b8d5f6aa1a66ea5
mass8 <29457120+h64m1@users.noreply.github.com>
2021-04-23 20:14:21 +0900

    feat(cell.tsx): init cell component
    - init commitizen

---
+++ renderer/src/components/cell/Cell.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type ContainerProps = {
+    stack?: number // 0 <= stack <= 1
+}
+
+type Props = Required<ContainerProps>
+
+export const Component: React.VFC<Props> = (props) => {
+    // TODO: convert props.stack to color
+    const color = '#000'
+    return (
+        <div style={{
+            backgroundColor: color
+        }}></div>
+    )
+}
+
