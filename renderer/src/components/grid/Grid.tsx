304c77efa06941471b104c0fcd277d404b090c40
mass8 <29457120+h64m1@users.noreply.github.com>
2021-04-30 15:49:00 +0900

    feat(grid): implement grid component #9
    - show cells with grid css | 1 column = 7 cells
    - add storybook for Grid

--- renderer/src/components/grid/Grid.tsx
+++ renderer/src/components/grid/Grid.tsx
@@ -1,7 +1,40 @@
-import { ColumnContainer, ColumnContainerProps } from "../column"
+import { CellContainer } from '../cell'
 
-type Props = ColumnContainerProps
+export type ContainerProps = {
+	/**
+	 * Array of stacks
+	 */
+	stacks: number[]
+}
+
+type Props = {
+	/**
+	 * What class to be used for Column
+	 */
+	className?: string
+} & ContainerProps
+
+export type StyledProps = Props
 
 const Component: React.VFC<Props> = (props) => {
-    return <ColumnContainer stacks={[1, 30, 60]} />
+	return (
+		<div className={props.className}>
+			{props.stacks.map((e, i) => (
+				<CellContainer key={i} stack={e} />
+			))}
+		</div>
+	)
+}
+
+export const StyledComponent: React.VFC<StyledProps> = (props) => {
+	return (
+		<Component
+			className="grid grid-flow-col grid-rows-7 auto-cols-min gap-1"
+			stacks={props.stacks}
+		/>
+	)
+}
+
+export const Container: React.VFC<ContainerProps> = (props) => {
+	return <StyledComponent stacks={props.stacks} />
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./renderer/pages/**/*.{js,ts,jsx,tsx}', './renderer/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        gridTemplateRows: {
+         '7': 'repeat(7, minmax(0, 1fr))',
+        }
+    },
   },
   variants: {
     extend: {},
