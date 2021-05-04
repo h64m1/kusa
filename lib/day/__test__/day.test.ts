dc6dca4b2fea53ae70046993cf17b812a01af1c8
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-04 18:53:08 +0900

    feat(day): add diff method to calculate number of date #11
    - between rhs and lhs
    - also add test for diff

--- lib/day/__test__/day.test.ts
+++ lib/day/__test__/day.test.ts
@@ -1,6 +1,7 @@
-import * as day from '../day'
+import { day } from '../'
 
 test('test day library', () => {
+    // Add
     // Long unit type
     expect(day.add('2021-04-05', 1, 'day')).toBe('2021-04-06')
     expect(day.add('2021-04-05', 1, 'month')).toBe('2021-05-05')
@@ -10,4 +11,8 @@ test('test day library', () => {
     expect(day.add('2021-05-02', 1, 'd')).toBe('2021-05-03')
     expect(day.add('2021-05-02', 1, 'M')).toBe('2021-06-02')
     expect(day.add('2021-05-02', 1, 'y')).toBe('2022-05-02')
+
+    // Difference
+    expect(day.diff('2021-04-01', '2020-04-01')).toBe(365)
+    expect(day.diff('2021-05-01', '2021-05-01')).toBe(0)
 })
--- lib/day/day.ts
+++ lib/day/day.ts
@@ -8,7 +8,18 @@ const FORMAT_DATE = 'YYYY-MM-DD'
  * @param {string} date date
  * @param {number} amount to be added
  * @param {UnitType} unit like day, month etc
+ * @return date
  */
 export const add = (date: string, amount: number, unit: dayjs.UnitType): string => {
     return dayjs(date).add(amount, unit).format(FORMAT_DATE)
+}
+
+/**
+ * Difference between lhs and rhs, return number of date
+ * @param {string} lhs subtrahend
+ * @param {string} rhs minuend
+ * @return number of date
+ */
+export const diff = (lhs: string, rhs: string): number => {
+    return dayjs(lhs).diff(rhs, 'day')
 }
