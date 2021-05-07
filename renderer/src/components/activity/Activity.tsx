00903afa986d1de67501b52bcac036e7b18447ac
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-07 19:48:21 +0900

    feat(grid): add week component #15

--- renderer/src/components/activity/Activity.tsx
+++ renderer/src/components/activity/Activity.tsx
@@ -1,18 +1,29 @@
 import { GridContainer, GridContainerProps } from '../grid'
+import { ContainerProps } from '../grid/Grid'
+import { WeekContainer } from '../week'
 
-type Props = GridContainerProps
-export type StyledProps = Props
+type Props = {
+	/**
+	 * What class to be used for Activity
+	 */
+	className?: string
+} & GridContainerProps
+
+export type StyledProps = ContainerProps
 
 const Component: React.VFC<Props> = (props) => {
 	return (
-		<GridContainer
-			beginDate={props.beginDate}
-			endDate={props.endDate}
-			stacks={props.stacks}
-		/>
+		<div className={props.className}>
+			<WeekContainer />
+			<GridContainer
+				beginDate={props.beginDate}
+				endDate={props.endDate}
+				stacks={props.stacks}
+			/>
+		</div>
 	)
 }
 
 export const StyledComponent: React.VFC<StyledProps> = (props) => {
-    return <Component {...props} />
+	return <Component className="flex flex-row space-x-1" {...props} />
 }
