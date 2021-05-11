c0973fd2e959d0e0b59c994d997cc561694c6c65
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-11 22:59:10 +0900

    feat(legend): add props to make legend horizontal #16

--- renderer/src/components/legend/Legend.tsx
+++ renderer/src/components/legend/Legend.tsx
@@ -1,21 +1,29 @@
 import { CellContainer } from '../cell'
 
-const Component: React.VFC = () => {
+type Props = {
+    /**
+     * What class to be used for Legend
+     */
+    className: string
+}
+
+const Component: React.VFC<Props> = (props) => {
+    const textSize = 'text-xs'
 	return (
-		<>
-			<div>Low</div>
+		<div className={props.className}>
+			<div className={textSize}>Low</div>
 			<CellContainer stack={0} />
 			<CellContainer stack={25} />
 			<CellContainer stack={50} />
 			<CellContainer stack={75} />
 			<CellContainer stack={100} />
-			<div>High</div>
-		</>
+			<div className={textSize}>High</div>
+        </div>
 	)
 }
 
 export const StyledComponent: React.VFC = () => {
-	return <Component />
+	return <Component className="flex flex-row space-x-1" />
 }
 
 export const Container: React.VFC = () => {
