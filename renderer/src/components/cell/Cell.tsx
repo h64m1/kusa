51242e42553e5745f9aa7c6c211fe777ad7d458d
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-15 18:32:41 +0900

    feat(cell): add hover event handler to cell component #18
    - add date, stack (to show these infomation in tooltip)
    - rename stack to stackNormalized

--- renderer/src/components/cell/Cell.tsx
+++ renderer/src/components/cell/Cell.tsx
@@ -1,11 +1,27 @@
+import { MouseEventHandler } from 'react'
 import 'tailwindcss/tailwind.css'
 import * as Presenter from './presenter'
 
+type CommonProps = {
+	/**
+	 * Event handler on focus
+	 */
+	onHover: MouseEventHandler<HTMLDivElement>
+}
+
 export type ContainerProps = {
+	/**
+	 * date (YYYY-MM-DD)
+	 */
+	date: string
+	/**
+	 * original stack value
+	 */
+	stack: number
 	/**
 	 * 0 <= stack <= 100
 	 */
-	stack: number
+	stackNormalized: number
 }
 
 type Props = {
@@ -13,29 +29,44 @@ type Props = {
 	 * What class to be used for cell
 	 */
 	className: string
-}
+} & CommonProps
 
 export type StyledProps = {
 	/**
 	 * What background color class
 	 */
 	backgroundColorClass: string
-}
+} & CommonProps
 
 const Component: React.VFC<Props> = (props) => {
-	return <div className={props.className}>&nbsp;</div>
+	return (
+		<div className={props.className} onMouseEnter={props.onHover}>
+			&nbsp;
+		</div>
+	)
 }
 
 export const StyledComponent: React.VFC<StyledProps> = (props) => {
 	const shapeProps = 'w-4 h-4 rounded border border-black'
 	const backgroundColor = props.backgroundColorClass
 
-	return <Component className={`${shapeProps} ${backgroundColor}`} />
+	return (
+		<Component
+			className={`${shapeProps} ${backgroundColor}`}
+			onHover={props.onHover}
+		/>
+	)
 }
 
 export const Container: React.VFC<ContainerProps> = (props) => {
 	// convert props.stack to background color
-	const backgroundColor = Presenter.backgroundColor(props.stack)
-
-	return <StyledComponent backgroundColorClass={backgroundColor} />
+	const backgroundColor = Presenter.backgroundColor(props.stackNormalized)
+	return (
+		<StyledComponent
+			backgroundColorClass={backgroundColor}
+			onHover={() => {
+				return console.debug('props', props)
+			}}
+		/>
+	)
 }
