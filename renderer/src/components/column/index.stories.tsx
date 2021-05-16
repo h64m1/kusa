31af2d78efe2472db461c8178880472ff65c5a3b
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-16 14:54:03 +0900

    feat(column): fix column component #18
    add date, and normalized stack props (array)

--- renderer/src/components/column/index.stories.tsx
+++ renderer/src/components/column/index.stories.tsx
@@ -1,17 +1,26 @@
-import { Story, Meta } from '@storybook/react';
+import { Meta, Story } from '@storybook/react'
 import { Column, ColumnProps } from './index'
 
 // export const Primary: React.VFC = () => <Cell backgroundColor={'#000'} />
 export default {
-  title: 'components/Column',
-  component: Column,
-} as Meta;
+	title: 'components/Column',
+	component: Column,
+} as Meta
 
-const Template: Story<ColumnProps> = (args) => <Column {...args} />;
-
+const Template: Story<ColumnProps> = (args) => <Column {...args} />
 
 // test
-export const ColumnTest = Template.bind({});
+export const ColumnTest = Template.bind({})
 ColumnTest.args = {
-    stacks: [0, 20, 40, 60, 80, 0, 80]
-};
+	dates: [
+		'2021-05-01',
+		'2021-05-02',
+		'2021-05-03',
+		'2021-05-04',
+		'2021-05-05',
+		'2021-05-06',
+		'2021-05-07',
+	],
+	stacks: [0, 20, 40, 60, 80, 0, 80],
+	stacksNormalized: [0, 20, 40, 60, 80, 0, 80],
+}
