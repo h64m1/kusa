11c29c84dd3b00e4de9366b4c1cfcd912103155d
mass8 <29457120+h64m1@users.noreply.github.com>
2021-05-17 15:23:19 +0900

    feat(input): add templates directory to storybook #24

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   "stories": [
     // "../stories/**/*.stories.mdx",
-    "../renderer/src/components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../renderer/src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../renderer/src/templates/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
