e904272291ca950d77781b581e2b2ac858941051
mass8 <29457120+h64m1@users.noreply.github.com>
2021-06-20 16:41:21 +0900

    feat(cell): add onClick event to cell #39

--- renderer/src/components/cell/Cell.tsx
+++ renderer/src/components/cell/Cell.tsx
@@ -1,5 +1,13 @@
+import { MouseEventHandler } from 'react'
 import 'tailwindcss/tailwind.css'
 import * as Presenter from './presenter'
+
+type CommonProps = {
+	/**
+	 * click action
+	 */
+	onClick: MouseEventHandler<HTMLDivElement>
+}
 
 export type ContainerProps = {
 	/**
@@ -14,6 +22,10 @@ export type ContainerProps = {
 	 * 0 <= stack <= 100
 	 */
 	stackNormalized: number
+	/**
+	 * click event handler: change date
+	 */
+	changeDate?: (date: string)  => void
 }
 
 type Props = {
@@ -21,28 +33,41 @@ type Props = {
 	 * What class to be used for cell
 	 */
 	className: string
-}
+} & CommonProps
 
 export type StyledProps = {
 	/**
 	 * What background color class
 	 */
 	backgroundColorClass: string
-}
+} & CommonProps
 
 const Component: React.VFC<Props> = (props) => {
-	return <div className={props.className}>&nbsp;</div>
+	return <div className={props.className}
+		onClick={props.onClick}
+	>&nbsp;</div>
 }
 
 export const StyledComponent: React.VFC<StyledProps> = (props) => {
 	const shapeProps = 'w-4 h-4 rounded border border-black'
 	const backgroundColor = props.backgroundColorClass
 
-	return <Component className={`${shapeProps} ${backgroundColor}`} />
+	return <Component className={`${shapeProps} ${backgroundColor}`}
+		onClick={props.onClick}
+	/>
 }
 
 export const Container: React.VFC<ContainerProps> = (props) => {
 	// convert props.stack to background color
 	const backgroundColor = Presenter.backgroundColor(props.stackNormalized)
-	return <StyledComponent backgroundColorClass={backgroundColor} />
+	return <StyledComponent backgroundColorClass={backgroundColor}
+		onClick={() => {
+			// skip if changeDate is undefined
+			if (props.changeDate === undefined) {
+				return
+			}
+			props.changeDate(props.date)
+			console.debug('onClick', props.date)
+		}}
+	/>
 }
