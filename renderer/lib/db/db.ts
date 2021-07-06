16bfdfa58336e2a7b0528096a2f8496bfd90d8f4
mass8 <29457120+h64m1@users.noreply.github.com>
2021-07-06 13:41:36 +0900

    feat(db): implement get, put in db #30

--- renderer/lib/db/db.ts
+++ renderer/lib/db/db.ts
@@ -1,5 +1,4 @@
-
-import localforage from "localforage"
+import localforage from 'localforage'
 import * as config from '../../utils/config'
 
 let _db: LocalForage
@@ -10,6 +9,40 @@ let _db: LocalForage
  */
 const create = (): LocalForage => {
 	return localforage.createInstance({
-		name: config.NAME
+		name: config.NAME,
 	})
+}
+
+/**
+ * Initialize db
+ */
+const init = () => {
+	if (_db !== undefined) {
+		return
+	}
+
+	// create new instance if current db is empty
+	_db = create()
+}
+
+/**
+ * Get value from db
+ * @param {string} key key
+ * @returns item
+ */
+export const get = (key: string): Promise<unknown> => {
+	// Init if db is empty
+	init()
+	return _db.getItem(key)
+}
+
+/**
+ * Set value to db with key
+ * @param {string} key key
+ * @param {unknown} value stored value
+ */
+export const put = (key: string, value: unknown) => {
+	// Init if db is empty
+	init()
+	_db.setItem(key, value)
 }
