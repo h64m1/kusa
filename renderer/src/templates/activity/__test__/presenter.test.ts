d0176124b4cde7d5a4b51111e8032b8eb09a7d50
mass8 <29457120+h64m1@users.noreply.github.com>
2021-07-10 14:18:23 +0900

    fix(grid): move months method from Activity to Grid #48

--- renderer/src/templates/activity/__test__/presenter.test.ts
+++ renderer/src/templates/activity/__test__/presenter.test.ts
@@ -1,18 +1,9 @@
 import * as Presenter from '../presenter'
 
 test('activity presenter: month names', () => {
-    // ['Jan', 'Feb']
-    expect(Presenter.months('2021-01-31', '2021-02-07')).toEqual(['Jan', 'Feb'])
-    // ['Mar', '', 'Apr']
-    expect(Presenter.months('2021-03-22', '2021-04-07')).toEqual(['Mar', '', 'Apr'])
-    // ['Sep', '', '', 'Oct']
-    expect(Presenter.months('2021-09-16', '2021-10-09')).toEqual(['Sep', '', '', 'Oct'])
-    // []
-    expect(Presenter.months('2021-04-01', '2021-02-01')).toEqual([])
-
     // test trim
     // ['', 'Jan', '']
-    expect(Presenter.trim(Presenter.months('2020-12-28', '2021-01-11'))).toEqual(['', 'Jan', ''])
+    expect(Presenter.trim(['Jan', 'Jan', ''])).toEqual(['', 'Jan', ''])
     // ['Dec', '', 'Jan', '']
-    expect(Presenter.trim(Presenter.months('2020-12-23', '2021-01-11'))).toEqual(['Dec', '', 'Jan', ''])
+    expect(Presenter.trim(['Dec', '', 'Jan', 'Jan'])).toEqual(['Dec', '', 'Jan', ''])
 })
--- renderer/src/templates/grid/presenter.ts
+++ renderer/src/templates/grid/presenter.ts
@@ -84,7 +84,80 @@ export const normalize = (stacks: number[]): number[] => {
  */
 export const hide = (dates: string[]): boolean[] => {
 	const today = day.today()
-	return dates.map(e => {
+	return dates.map((e) => {
 		return day.diff(today, e) < 0
 	})
+}
+
+/**
+ * Create months (Jan, Feb, ...)
+ * @param {string} beginDate begin date for grid cells
+ * @param {string} endDate end date for grid cells
+ * @returns array of months
+ */
+export const months = (beginDate: string, endDate: string): string[] => {
+	const dates = gridCells(beginDate, endDate)
+
+	const months: string[] = []
+	const length = dates.length
+	let currentMonth = -1
+	for (let i = 0; i < length; i++) {
+		const date = dates[i]
+		if (i % 7 === 0) {
+			const month = getMonthName(date, currentMonth)
+			months.push(month.name)
+			currentMonth = month.monthId
+		}
+	}
+
+	return months
+}
+
+type MonthName = {
+	/**
+	 * Month name
+	 */
+	name: string
+	/**
+	 * Month index
+	 */
+	monthId: number
+}
+
+const monthNames = [
+	'Jan',
+	'Feb',
+	'Mar',
+	'Apr',
+	'May',
+	'Jun',
+	'Jul',
+	'Aug',
+	'Sep',
+	'Oct',
+	'Nov',
+	'Dec',
+]
+
+/**
+ * Pair of (month name, month index)
+ * @param {string} date date
+ * @param {number} currentMonth current month index
+ * @returns pair of (month name, month index)
+ */
+const getMonthName = (date: string, currentMonth: number): MonthName => {
+	const month = day.month(date)
+	if (month !== currentMonth) {
+		// new month is found
+		return {
+			name: monthNames[month],
+			monthId: month,
+		}
+	} else {
+		// same month, do not change name/month
+		return {
+			name: '',
+			monthId: currentMonth,
+		}
+	}
 }
