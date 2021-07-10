145934c4119fb2589e858190590d5c3d8af37578
mass8 <29457120+h64m1@users.noreply.github.com>
2021-07-10 14:21:37 +0900

    fix(card): fix initial value for Card unit test #48

--- renderer/src/templates/card/__test__/presenter.test.ts
+++ renderer/src/templates/card/__test__/presenter.test.ts
@@ -2,7 +2,7 @@ import { act, renderHook } from '@testing-library/react-hooks'
 import * as Presenter from '../presenter'
 
 test('activities custom hook', () => {
-    const { result } = renderHook(() => Presenter.useActivities([]))
+    const { result } = renderHook(() => Presenter.useActivities(''))
 
     // initial value
     expect(result.current.activities).toStrictEqual([])
